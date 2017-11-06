3af60e911b9aff6ff172302a47d5086d714ed68b
Paula <pauladelahoz@correo.ugr.es>
2017-11-06 14:10:24 -0500

    flaskeando

--- pruebas/FlaskApp/app.py
+++ pruebas/FlaskApp/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 def main():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
