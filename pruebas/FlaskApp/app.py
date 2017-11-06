062920e07401aa2c9da8c297ff79e8264014b233
Paula <pauladelahoz@correo.ugr.es>
2017-11-06 13:53:48 -0500

    flaskeando mas

---
+++ pruebas/FlaskApp/app.py
@@ -0,0 +1,15 @@
+# -*- coding: utf- 8 -*-
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+
+
+def main():
+    return render_template('index2.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
+    # escucha a cualquier interfaz, desde cualquier sitio y (modo debug) SOLO EN DEV, NO EN PRODUCCION
