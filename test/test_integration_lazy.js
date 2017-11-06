472f3a6363b048f0e895145e971c846948a9b834
Paula <pauladelahoz@correo.ugr.es>
2017-11-06 14:51:37 -0500

    Comento una mierda, y odio a Germán. Closes #2.

--- test/test_integration_lazy.js
+++ test/test_integration_lazy.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import MyComponent from '/tiempoReal/index.html'
 // helper function that mounts and returns the rendered text
+//este ya no
 function getRenderedText (Component, propsData) {
   const Ctor = Vue.extend(Component)
   const vm = new Ctor({ propsData: propsData }).$mount()
