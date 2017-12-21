c37e7f37515025323ed33f3a8fa7ca71d5f91b73
Paula <pauladelahoz@correo.ugr.es>
2017-12-21 05:14:39 -0500

    proxys de diferentes paises

--- tiempoReal/conproxy.py
+++ tiempoReal/conproxy.py
@@ -2,14 +2,16 @@ import urllib2
 import urllib
 import random
 
-CAproxies = [{"http":"199.201.122.175:3128", "https":"199.201.122.175:3128"},{"http":"192.99.3.129:3128", "https":"192.99.3.129:3128"},{"http":"192.99.246.101:8118", "https":"192.99.246.101:8118"},{"http":"205.205.129.130:443", "https":"205.205.129.130:443"} ]
+# CHINA {http, https},  ISRAEL {http, https}
+
+CAproxies = [{"http":"222.161.137.204:9797", "https":"111.40.84.73:9999"},{"http":"192.117.146.110:80", "https":"31.168.230.194:8080"},{"http":"192.99.246.101:8118", "https":"192.99.246.101:8118"},{"http":"205.205.129.130:443", "https":"205.205.129.130:443"} ]
 
 # http://www.proxy-listen.de/Proxy/Proxyliste.html
 # https://stackoverflow.com/questions/30292361/request-from-different-country-ip
 
 proxies = urllib2.ProxyHandler(random.choice(CAproxies))
 
-urls =[{"www.facebook.com","www.twitter.com","www.wikipedia.com"}]
+urls =[{"www.facebook.com","www.twitter.com","www.wikipedia.com"},{OTRAS}]
 url = urls[1];
 # hacer un for con url para comprobar uno a uno
 
